91db032dfb5795c132c39a0a3ca7927545e62763
muratslavich <slavichmo@gmail.com>
2020-11-24 02:06:01 +0300

    fix

--- lesson-3-proxy/src/main/java/annotationproxy/SuspectedServiceImpl.java
+++ lesson-3-proxy/src/main/java/annotationproxy/SuspectedServiceImpl.java
@@ -11,6 +11,7 @@ public class SuspectedServiceImpl implements SuspectedService {
     @Aspectional
     public void foo() {
         log.info("----> foo");
+        this.bar();
     }
 
     @Aspectional
